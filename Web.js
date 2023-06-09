27a801cb653b36d68521974b2e51d8f0f0c57fcf
Pyrobyte <Pyro.Byte@outlook.com>
2023-06-09 12:05:54 +0100

    Create Web.js

---
+++ Web.js
@@ -0,0 +1,74 @@
+<!doctype html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+
+        <title>Pyrobyte.com</title>
+
+        <!-- Load external CSS styles -->
+        <link rel="stylesheet" href="styles.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+</style>
+</head>
+<body>
+
+<!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+    <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CONTACT</a>
+    <div class="w3-dropdown-hover w3-hide-small">
+      <button class="w3-padding-large w3-button" title="More">MORE <i class="fa fa-caret-down"></i></button>     
+      <div class="w3-dropdown-content w3-bar-block w3-card-4">
+        <a href="#" class="w3-bar-item w3-button">Merchandise</a>
+        <a href="#" class="w3-bar-item w3-button">Extras</a>
+        <a href="#" class="w3-bar-item w3-button">Media</a>
+      </div>
+    </div>
+    <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+  </div>
+</div>
+
+<!-- Navbar on small screens (remove the onclick attribute if you want the navbar to always show on top of the content when clicking on the links) -->
+<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
+  <a href="#code" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">CODE</a>
+  <a href="#learn" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">LEARN</a>
+  <a href="#contact" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">CONTACT</a>
+  <a href="#" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">MERCH</a>
+</div>
+
+<!-- Page content -->
+<div class="w3-content" style="max-width:2000px;margin-top:46px">
+
+ 
+  <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+    </head>
+
+    <body>
+
+        <b><h1>Pyrobyte's Homepage</h1> </b>
+
+    <script src="https://openchat.so/chat.js"></script>
+<script>
+    var chatConfig = {
+    token: "sD0OCh5HDsrgIF27zlfU",
+    };
+    initializeChatWidget(chatConfig);
+</script>
+                                                
+        <!-- Load external JavaScript -->
+        <script src="scripts.js"></script>
+        <button><a href="https://github.com/dragon731012/nothing2/archive/refs/heads/master.zip"</a>download loads of games here!</button>
+        
+    </body>
+
+</html>
